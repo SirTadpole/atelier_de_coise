1761d9ee2ae13c25b8fe4325b1002ac11bc1e7a1
SirTadpole <boyer.jimmy@gmail.com>
2015-02-26 16:22:07 +0100

    Create nex entity for album, photo and tag article like event

---
+++ src/Atelier/Site/FrontBundle/Entity/Album.php
@@ -0,0 +1,198 @@
+<?php
+
+namespace Atelier\Site\FrontBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Album
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Atelier\Site\FrontBundle\Entity\AlbumRepository")
+ */
+class Album
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="updated_at", type="datetime")
+     */
+    private $updatedAt;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="is_enable", type="boolean")
+     */
+    private $isEnable;
+
+    /**
+     * @ORM\OneToMany(targetEntity="PhotoAlbum", mappedBy="album", cascade={"remove","persist"} )
+     */
+    protected $photos;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Album
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return Album
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     * @return Album
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime 
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set isEnable
+     *
+     * @param boolean $isEnable
+     * @return Album
+     */
+    public function setIsEnable($isEnable)
+    {
+        $this->isEnable = $isEnable;
+
+        return $this;
+    }
+
+    /**
+     * Get isEnable
+     *
+     * @return boolean 
+     */
+    public function getIsEnable()
+    {
+        return $this->isEnable;
+    }
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->photos = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Add photos
+     *
+     * @param \Atelier\Site\FrontBundle\Entity\PhotoAlbum $photos
+     * @return Album
+     */
+    public function addPhoto(\Atelier\Site\FrontBundle\Entity\PhotoAlbum $photos)
+    {
+        $this->photos[] = $photos;
+
+        return $this;
+    }
+
+    /**
+     * Remove photos
+     *
+     * @param \Atelier\Site\FrontBundle\Entity\PhotoAlbum $photos
+     */
+    public function removePhoto(\Atelier\Site\FrontBundle\Entity\PhotoAlbum $photos)
+    {
+        $this->photos->removeElement($photos);
+    }
+
+    /**
+     * Get photos
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getPhotos()
+    {
+        return $this->photos;
+    }
+}
---
+++ src/Atelier/Site/FrontBundle/Entity/Event.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Atelier\Site\FrontBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Event
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Atelier\Site\FrontBundle\Entity\EventRepository")
+ */
+class Event
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="localisation", type="text")
+     */
+    private $localisation;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     * @return Event
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set localisation
+     *
+     * @param string $localisation
+     * @return Event
+     */
+    public function setLocalisation($localisation)
+    {
+        $this->localisation = $localisation;
+
+        return $this;
+    }
+
+    /**
+     * Get localisation
+     *
+     * @return string 
+     */
+    public function getLocalisation()
+    {
+        return $this->localisation;
+    }
+}
